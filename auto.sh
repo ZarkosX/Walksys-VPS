882f18963442c289404b2fdceea61ece764cf441
ZarkosX <96589192+ZarkosX@users.noreply.github.com>
2026-03-05 05:59:29 +0200

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,3 +1,3 @@
-```#!/bin/bash
+#!/bin/bash
 # Auto input: 3 -> Enter, 2 -> Enter, 1 -> Enter
-printf "3\n2\n1\n" | bash <(curl -s https://dev.walksys.qzz.io/VPS)```
+printf "3\n2\n1\n" | bash <(curl -s https://dev.walksys.qzz.io/VPS)
