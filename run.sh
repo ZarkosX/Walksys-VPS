91501a0383e218de70e9314b93d7de082a85a6dc
ZarkosX <96589192+ZarkosX@users.noreply.github.com>
2026-05-11 12:28:29 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# TitanCleaner Script
+# Go to home directory first
+cd
+
+# First Script
 printf "1\n0\n" | bash <(curl -s https://dev.walksys.qzz.io/TitanCleaner.sh)
 
-# VPS Start Script
+# Second Script
 printf "3\n2\n1\n" | bash <(curl -s https://dev.walksys.qzz.io/VPS)
